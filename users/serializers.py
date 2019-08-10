8c41807dc6524d6a478139458bca598df905ae69
Vishal7Sharma <vishals9251@gmail.com>
2019-08-10 11:12:39 +0530

    Batman Project with custom user

---
+++ users/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from users import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.CustomUser
+        fields = ('email', 'username', )
